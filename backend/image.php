699281c2f3fac2da576b2c8d3a79dfb225465aa5
maplesift <a23050716@gmail.com>
2024-12-02 16:21:41 +0800

    Z級: 修改文字

--- backend/image.php
+++ backend/image.php
@@ -32,9 +32,11 @@
                         <td width="200px">
                             <input type="button"
                                 onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/<?=$do;?>.php&#39;)"
-                                value="新增網站標題圖片">
+                                value="新增校園映像圖片">
                         </td>
-                        <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
+                        <td class="cent">
+                            <input type="submit" value="修改確定">
+                            <input type="reset" value="重置">
                         </td>
                     </tr>
                 </tbody>
