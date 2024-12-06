5184f8e7f4d970be1a7049819350ffe4242914e8
maplesift <a23050716@gmail.com>
2024-12-06 10:32:06 +0800

    z級: 修改全域函式 pdo

--- api/db.php
+++ api/db.php
@@ -146,7 +146,7 @@ class DB{
 
 function q($sql){
     $pdo=new PDO("mysql:host=localhost;charset=utf8;dbname=db03",'root','');
-    return $this->pdo->query($sql)->fetchAll();
+    return $pdo->query($sql)->fetchAll();
 }
 
 function dd($array){
