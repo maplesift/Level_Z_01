4b14d68829a984e7eca01cf38bb97096c8fb19dc
maplesift <a23050716@gmail.com>
2024-12-09 15:46:13 +0800

    z級:撰寫更多次選單的js功能

--- modal/submenu.php
+++ modal/submenu.php
@@ -2,7 +2,7 @@
 <h3 class="cent">編輯次選單</h3>
 <hr>
 <form action="api/insert.php" method="post" enctype="multipart/form-data" class="cent">
-    <table style="width:50%;margin:auto;">
+    <table style="width:50%;margin:auto;" id="menu">
         <tr>
             <td>次選單名稱：</td>
             <td>次選單連結網址：</td>
@@ -20,6 +20,28 @@
 
         <input type="submit" value="修改確定">
         <input type="reset" value="重置">
-        <input type="button" value="更多次選單">
+        <input type="button" value="更多次選單" onclick="more()">
     </div>
 </form>
+
+<script>
+// $("#menu").append(`
+//             <tr>
+//             <td><input type="text" name="text" id="text"></td>
+//             <td><input type="text" name="href" id="href"></td>
+//             <td></td>
+//         </tr>
+//     `)
+
+function more() {
+    let row = `
+            <tr>
+            <td><input type="text" name="text" id="text"></td>
+            <td><input type="text" name="href" id="href"></td>
+            <td></td>
+            </tr>`
+    $("#menu").append(row);
+
+
+}
+</script>
